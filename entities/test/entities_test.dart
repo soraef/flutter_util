9f578c5c1f5170dfc4d18b68485cdc4527ad5d9f
soraef <sorasorasorayama@gmail.com>
2021-10-19 10:44:46 +0900

    add license

--- entities/test/entities_test.dart
+++ entities/test/entities_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2021 Sora Fukui. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:entities/entities.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_test/flutter_test.dart';
